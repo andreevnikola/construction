5e4e2aaeedc6482b48374bddd30bd9b53b343817
andreevnikola <andr.nikola.08@gmail.com>
2025-04-03 23:48:14 +0300

    fixed employee page

--- web-client/src/pages/Employee.tsx
+++ web-client/src/pages/Employee.tsx
@@ -7,7 +7,7 @@ export default function Employee() {
   // We can use the `useParams` hook here to access
   // the dynamic pieces of the URL.
   let { id } = useParams();
-  const [employee, setEmployee] = useState<IEmployee | null>(null);
+  const [employee, setEmployee] = useState<IEmployee | null | false>(null);
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -17,7 +17,8 @@ export default function Employee() {
           .collection("employees")
           .getOne<IEmployee>(id || "");
         setEmployee(employeeData);
-      } catch (error) {
+      } catch (error: any) {
+        if (error.response.status === 404) setEmployee(false);
         console.error("Error fetching employee:", error);
       }
     };
@@ -43,56 +44,60 @@ export default function Employee() {
 
   return (
     <>
-      {employee ? (
-        <>
-          <div className="breadcrumbs text-sm">
-            <ul>
-              <li>
-                <Link to={"/"}>Records</Link>
-              </li>
-              <li>
-                <Link to={"/employees"}>Employees</Link>
-              </li>
-              <li>{employee.name}</li>
-            </ul>
-          </div>
-          <section className="flex flex-col items-center gap-5 border-2 border-primary w-full px-10 py-10 rounded-xl bg-base-100">
-            <div className="flex flex-col gap-6  w-fit ">
-              <h2 className="text-3xl font-semibold border-l-8 -ml-6 pl-3 border-accent pr-20">
-                Employee{" "}
-                <u className="decoration-primary font-extrabold">details</u>:
-              </h2>
-              <div className="flex flex-col gap-5 text-lg">
-                <p className="flex justify-between">
-                  <strong>Name:</strong> <span>{employee.name}</span>
-                </p>
-                <p className="flex justify-between">
-                  <strong>Position:</strong> <span>{employee.position}</span>
-                </p>
-                <p className="flex justify-between">
-                  <strong>Wage:</strong> <span>{employee.wage} BGN</span>
-                </p>
-                <p className="flex justify-between">
-                  <strong>Baseline Income:</strong>{" "}
-                  {employee.baseline_income
-                    ? employee.baseline_income + " BGN"
-                    : "N/A"}
-                </p>
+      <div className="breadcrumbs text-sm">
+        <ul>
+          <li>
+            <Link to={"/"}>Records</Link>
+          </li>
+          <li>
+            <Link to={"/employees"}>Employees</Link>
+          </li>
+          <li>{employee && employee.name ? employee.name : "Employee"}</li>
+        </ul>
+      </div>
+      {employee !== null ? (
+        employee === false ? (
+          <p>There is no such employee!</p>
+        ) : (
+          <>
+            <section className="flex flex-col items-center gap-5 border-2 border-primary w-full px-10 py-10 rounded-xl bg-base-100">
+              <div className="flex flex-col gap-6  w-fit ">
+                <h2 className="text-3xl font-semibold border-l-8 -ml-6 pl-3 border-accent pr-20">
+                  Employee{" "}
+                  <u className="decoration-primary font-extrabold">details</u>:
+                </h2>
+                <div className="flex flex-col gap-5 text-lg">
+                  <p className="flex justify-between">
+                    <strong>Name:</strong> <span>{employee.name}</span>
+                  </p>
+                  <p className="flex justify-between">
+                    <strong>Position:</strong> <span>{employee.position}</span>
+                  </p>
+                  <p className="flex justify-between">
+                    <strong>Wage:</strong> <span>{employee.wage} BGN</span>
+                  </p>
+                  <p className="flex justify-between">
+                    <strong>Baseline Income:</strong>{" "}
+                    {employee.baseline_income
+                      ? employee.baseline_income + " BGN"
+                      : "N/A"}
+                  </p>
+                </div>
+                <div className="flex w-full gap-2">
+                  <button className="btn flex grow " disabled>
+                    Update
+                  </button>
+                  <button
+                    className="btn btn-error flex grow"
+                    onClick={handleDelete}
+                  >
+                    Delete
+                  </button>
+                </div>
               </div>
-              <div className="flex w-full gap-2">
-                <button className="btn flex grow " disabled>
-                  Update
-                </button>
-                <button
-                  className="btn btn-error flex grow"
-                  onClick={handleDelete}
-                >
-                  Delete
-                </button>
-              </div>
-            </div>
-          </section>
-        </>
+            </section>
+          </>
+        )
       ) : (
         <p>Loading...</p>
       )}
